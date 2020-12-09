0d7bb8c868e34b14530c3d99362dd95abe0bacda
Mathijs Bernson <mathijsb@q42.nl>
2020-12-09 12:06:08 +0100

    wip

--- Sources/AdventOfCode2020/Day7/Day7.swift
+++ Sources/AdventOfCode2020/Day7/Day7.swift
@@ -10,7 +10,7 @@ public struct Day7 {
     let inputString = "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags."
     let bags = inputString.components(separatedBy: "\n")
       .filter({ !$0.isEmpty })
-      .compactMap(makeBag)
+      .compactMap(makeBagsLine)
     print(bags)
     return 0
   }
@@ -19,7 +19,14 @@ public struct Day7 {
     return 0
   }
 
-  func makeBag(from string: String) -> Bag? {
+  func makeBagsTree(from string: String) -> Bag {
+    let bags = string.components(separatedBy: "\n")
+      .filter({ !$0.isEmpty })
+      .compactMap(makeBagsLine)
+    return Bag(name: "__ROOT__", children: bags)
+  }
+
+  func makeBagsLine(from string: String) -> Bag {
     print(string)
     let parts = string.split(separator: " ")
     let parentName = parts[0...1].joined(separator: " ")
@@ -27,12 +34,44 @@ public struct Day7 {
     if string.contains("no other bags.") {
       return Bag(name: parentName, children: [])
     } else {
-      return nil
+      // ["1 bright white bag", "2 muted yellow bags."]
+      let childrenStrings = parts[4...].joined(separator: " ")
+        .components(separatedBy: ", ")
+      let children: [Bag] = childrenStrings.flatMap { string -> [Bag] in
+        let components = string.components(separatedBy: " ")
+        let quantity = Int(components[0]) ?? 1
+        let childName = components[1...2].joined(separator: " ")
+        return (0..<quantity).map { _ in
+          Bag(name: childName, children: [])
+        }
+      }
+      return Bag(name: parentName, children: children)
     }
   }
 
-  struct Bag {
+  struct Bag: Equatable {
     let name: String
-    let children: [Bag]
+    var children: [Bag]
+
+    func canContain(_ other: Bag) -> Bool {
+      return children.map(\.name).contains(other.name)
+    }
+
+    func deepContains(_ other: Bag) -> [Bag] {
+      children.filter { $0.canContain(other) } +
+        children.flatMap { $0.deepContains(other) }
+    }
+
+    mutating func deepAppend(_ other: Bag, to name: String) {
+      if self.name == name {
+        children.append(other)
+      }
+
+      self.children = children.map { _child -> Bag in
+        var child = _child
+        child.deepAppend(other, to: name)
+        return child
+      }
+    }
   }
 }
--- Tests/AdventOfCode2020Tests/Day7Tests.swift
+++ Tests/AdventOfCode2020Tests/Day7Tests.swift
@@ -2,4 +2,73 @@
 import XCTest
 
 final class Day7Tests: XCTestCase {
+  func testMakeBagsNoOther() throws {
+    XCTAssertEqual(
+      Day7().makeBagsLine(from: "dotted black bags contain no other bags."),
+      Day7.Bag(name: "dotted black", children: [])
+    )
+  }
+
+  func testMakeBags() throws {
+    XCTAssertEqual(
+      Day7().makeBagsLine(from: "light red bags contain 1 bright white bag, 2 muted yellow bags."),
+      Day7.Bag(name: "light red", children: [
+        Day7.Bag(name: "bright white", children: []),
+        Day7.Bag(name: "muted yellow", children: []),
+        Day7.Bag(name: "muted yellow", children: []),
+      ])
+    )
+  }
+
+  func testMakeBagsTree() throws {
+    let inputString = "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag."
+
+    // light red bags contain 1 bright white bag, 2 muted yellow bags.
+    // dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    // bright white bags contain 1 shiny gold bag.
+    XCTAssertEqual(
+      Day7().makeBagsTree(from: inputString),
+      Day7.Bag(name: "__ROOT__", children: [
+        Day7.Bag(name: "light red", children: [
+          Day7.Bag(name: "bright white", children: [
+            Day7.Bag(name: "shiny gold", children: []),
+          ]),
+          Day7.Bag(name: "muted yellow", children: []),
+          Day7.Bag(name: "muted yellow", children: []),
+        ]),
+        Day7.Bag(name: "dark orange", children: [
+          Day7.Bag(name: "bright white", children: [
+            Day7.Bag(name: "shiny gold", children: []),
+          ]),
+          Day7.Bag(name: "bright white", children: [
+            Day7.Bag(name: "shiny gold", children: []),
+          ]),
+          Day7.Bag(name: "bright white", children: [
+            Day7.Bag(name: "shiny gold", children: []),
+          ]),
+          Day7.Bag(name: "muted yellow", children: []),
+          Day7.Bag(name: "muted yellow", children: []),
+          Day7.Bag(name: "muted yellow", children: []),
+          Day7.Bag(name: "muted yellow", children: []),
+        ]),
+
+
+        Day7.Bag(name: "bright white", children: [
+          Day7.Bag(name: "shiny gold", children: []),
+        ]),
+      ])
+    )
+  }
+
+  func testTreeSearch() throws {
+    let inputString = "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags."
+    let tree = Day7().makeBagsTree(from: inputString)
+    let searchBag = Day7.Bag(name: "shiny gold", children: [])
+    XCTAssertEqual(tree.deepContains(searchBag).map(\.name), [
+      "bright white",
+      "muted yellow",
+      "dark orange",
+      "light red",
+    ])
+  }
 }
