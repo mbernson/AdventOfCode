e4a91f2d99f7c6e425ee5826344b53edd2f7450a
Mathijs Bernson <mathijsb@q42.nl>
2021-12-12 20:31:47 +0100

    Delete code

--- Sources/AdventOfCode2021/Day8/Day8.swift
+++ Sources/AdventOfCode2021/Day8/Day8.swift
@@ -3,42 +3,7 @@ import Foundation
 public struct Day8 {
   public let inputURL = Bundle.module.url(forResource: "day8", withExtension: "txt")!
 
-  typealias SegmentDisplay = Set<String.Element>
-
-  enum Segment {
-    static let zero: Day8.SegmentDisplay = ["a","b","c","e","f","g"]
-    static let one: Day8.SegmentDisplay = ["c","f"]
-    static let two: Day8.SegmentDisplay = ["a","c","d","e","g"]
-    static let three: Day8.SegmentDisplay = ["a","c","d","f","g"]
-    static let four: Day8.SegmentDisplay = ["b","c","d","f"]
-    static let five: Day8.SegmentDisplay = ["a","b","d","f","g"]
-    static let six: Day8.SegmentDisplay = ["a","b","d","e","f","g"]
-    static let seven: Day8.SegmentDisplay = ["a","c","f"]
-    static let eight: Day8.SegmentDisplay = ["a","b","c","d","e","f","g"]
-    static let nine: Day8.SegmentDisplay = ["a","b","c","d","f","g"]
-  }
-
-  private let segmentsToDigit: [Day8.SegmentDisplay: Int] = [
-    Segment.zero:  0,
-    Segment.one:   1,
-    Segment.two:   2,
-    Segment.three: 3,
-    Segment.four:  4,
-    Segment.five:  5,
-    Segment.six:   6,
-    Segment.seven: 7,
-    Segment.eight: 8,
-    Segment.nine:  9,
-  ]
-
-  private var segments: [SegmentDisplay: Int] = [:]
-
-  public init() {
-  }
-
-  func digitForSegments(_ segments: SegmentDisplay) -> Int? {
-    return segmentsToDigit[segments]
-  }
+  public init() {}
 
   public func runPart1() throws -> Int {
     let inputString = try String(contentsOf: inputURL)
@@ -60,8 +25,8 @@ public struct Day8 {
   }
 
   struct Entry {
-    let signalPatterns: [SegmentDisplay]
-    let outputValues: [SegmentDisplay]
+    let signalPatterns: [Set<String.Element>]
+    let outputValues: [Set<String.Element>]
 
     init?(line: String) {
       let parts = line.components(separatedBy: "|")
