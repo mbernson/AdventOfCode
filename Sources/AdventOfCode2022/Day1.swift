4486dbcd17b6d43d875a62bfd1055b135a976d02
Mathijs Bernson <mathijsb@q42.nl>
2022-12-01 08:09:30 +0100

    2022 day 1 part 2

--- Sources/AdventOfCode2022/Day1.swift
+++ Sources/AdventOfCode2022/Day1.swift
@@ -27,10 +27,24 @@ public struct Day1 {
 
     public func runPart2() throws -> Int {
         let inputString = try String(contentsOf: inputURL)
-        let input: [Int] = inputString
-            .components(separatedBy: "\n")
-            .compactMap(Int.init)
-        print(input)
-        return 0
+    
+        let input: [[Int]] = inputString
+            .components(separatedBy: "\n\n")
+            .map { string in
+                string
+                    .components(separatedBy: "\n")
+                    .compactMap(Int.init)
+            }
+
+        let sums: [Int] = input
+            .map { elf in
+                let total: Int = elf.reduce(0, +)
+                return total
+            }
+            .sorted(by: { $0 > $1 })
+        let result: Int = Array(sums[0...2])
+            .reduce(0, +)
+
+        return result
     }
 }
