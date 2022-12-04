b812c6f3af38781e00ab8fb516a2c7ff284feee8
Mathijs Bernson <mathijsb@q42.nl>
2022-12-04 12:14:04 +0100

    2022 day 4 part 2

--- Sources/AdventOfCode2022/Day4.swift
+++ Sources/AdventOfCode2022/Day4.swift
@@ -28,8 +28,23 @@ public struct Day4 {
     }
 
     public func runPart2() throws -> Int {
-        let inputString = try String(contentsOf: inputURL)
+        let lines: [String] = try String(contentsOf: inputURL)
+            .components(separatedBy: "\n")
+            .filter { !$0.isEmpty }
 
-        return 0
+        var result: Int = 0
+
+        for line in lines {
+            let parts = line.components(separatedBy: ",")
+            let a: [Int] = parts[0].components(separatedBy: "-").map { Int($0)! }
+            let range1 = a[0]...a[1]
+            let b: [Int] = parts[1].components(separatedBy: "-").map { Int($0)! }
+            let range2 = b[0]...b[1]
+            if range1.overlaps(range2) || range2.overlaps(range1) {
+                result += 1
+            }
+        }
+
+        return result
     }
 }
