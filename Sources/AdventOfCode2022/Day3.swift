49882f30117ec74b5a4df3ac0b64221113130eb1
Mathijs Bernson <mathijsb@q42.nl>
2022-12-04 11:27:52 +0100

    2022 day 3 part 2

--- Sources/AdventOfCode2022/Day3.swift
+++ Sources/AdventOfCode2022/Day3.swift
@@ -3,18 +3,20 @@ import Foundation
 public struct Day3 {
     public let inputURL = Bundle.module.url(forResource: "Input/day3", withExtension: "txt")!
 
-    public init() {}
+    let priorities: [Character: Int]
 
-    public func runPart1() throws -> Int {
+    public init() {
         var priorities: [Character: Int] = [:]
-
         for (character, priority) in zip(("a"..."z").characters, 1...26) {
             priorities[character] = priority
         }
         for (character, priority) in zip(("A"..."Z").characters, 27...52) {
             priorities[character] = priority
         }
+        self.priorities = priorities
+    }
 
+    public func runPart1() throws -> Int {
         let lines: [String] = try String(contentsOf: inputURL)
             .components(separatedBy: "\n")
             .filter { !$0.isEmpty }
@@ -37,6 +39,26 @@ public struct Day3 {
     }
 
     public func runPart2() throws -> Int {
-        return 0
+        let lines: [String] = try String(contentsOf: inputURL)
+            .components(separatedBy: "\n")
+            .filter { !$0.isEmpty }
+
+        var score: Int = 0
+        var iterator = stride(from: lines.startIndex, to: lines.endIndex, by: 3).makeIterator()
+        while let startIndex = iterator.next() {
+            let endIndex = min(startIndex.advanced(by: 3), lines.endIndex)
+            let set: Set<Character> = lines[startIndex..<endIndex].map { line in
+                Set(line.map(Character.init))
+            }.reduce(into: Set<Character>()) { result, item in
+                if result.isEmpty {
+                    result = result.union(item)
+                } else {
+                    result = result.intersection(item)
+                }
+            }
+            score += priorities[set.first!]!
+        }
+
+        return score
     }
 }
