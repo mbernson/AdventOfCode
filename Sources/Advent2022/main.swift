903021c6817bfab1a8c7ed2165fec16385b2f9ff
Mathijs Bernson <mathijsb@q42.nl>
2022-12-04 11:39:26 +0100

    2022 day 4 part 1

--- Sources/Advent2022/main.swift
+++ Sources/Advent2022/main.swift
@@ -16,6 +16,10 @@ case ["day3", "part1"]:
     print(try! Day3().runPart1())
 case ["day3", "part2"]:
     print(try! Day3().runPart2())
+case ["day4", "part1"]:
+    print(try! Day4().runPart1())
+case ["day4", "part2"]:
+    print(try! Day4().runPart2())
 
 case []:
     print("No subcommand given"); exit(1)
---
+++ Sources/AdventOfCode2022/Day4.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct Day4 {
+    public let inputURL = Bundle.module.url(forResource: "Input/day4", withExtension: "txt")!
+
+    public init() {}
+
+    public func runPart1() throws -> Int {
+        let lines: [String] = try String(contentsOf: inputURL)
+            .components(separatedBy: "\n")
+            .filter { !$0.isEmpty }
+
+        var result: Int = 0
+
+        for line in lines {
+            let parts = line.components(separatedBy: ",")
+            let a: [Int] = parts[0].components(separatedBy: "-").map { Int($0)! }
+            let range1 = a[0]...a[1]
+            let b: [Int] = parts[1].components(separatedBy: "-").map { Int($0)! }
+            let range2 = b[0]...b[1]
+            if (range1.contains(range2.lowerBound) && range1.contains(range2.upperBound)) ||
+               (range2.contains(range1.lowerBound) && range2.contains(range1.upperBound)) {
+                result += 1
+            }
+        }
+
+        return result
+    }
+
+    public func runPart2() throws -> Int {
+        let inputString = try String(contentsOf: inputURL)
+
+        return 0
+    }
+}
