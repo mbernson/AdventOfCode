b71f59d12d1c784e3a8491e123176ae7da2cc735
Mathijs Bernson <mathijsb@q42.nl>
2023-03-12 13:37:44 +0100

    2022 day 12 part 2

--- Sources/AdventOfCode2022/Day12.swift
+++ Sources/AdventOfCode2022/Day12.swift
@@ -78,6 +78,27 @@ public struct Day12 {
     }
 
     public func runPart2() -> Int {
-        return 42
+        var grid = self.grid
+        let startIndex = grid.memory.firstIndex(of: Character("S").asciiValue!)!
+        let endIndex = grid.memory.firstIndex(of: Character("E").asciiValue!)!
+        let end = grid.point(at: endIndex)
+        grid.memory[startIndex] = Character("a").asciiValue!
+        grid.memory[endIndex] = Character("z").asciiValue!
+        let startValue = Character("a").asciiValue!
+        let startingPoints: [Point] = grid.points.filter { point in
+            grid[point.x, point.y] == startValue
+        }
+        var solutions: [Int] = []
+        let semaphore = DispatchSemaphore(value: 1)
+        // Brute-force it by spreading the shortest path calculation out over all CPU cores
+        DispatchQueue.concurrentPerform(iterations: startingPoints.count) { index in
+            let start = startingPoints[index]
+            let shortestDistance = solve(grid: grid, start: start, end: end)
+            semaphore.wait()
+            solutions.append(shortestDistance)
+            semaphore.signal()
+            print("\(solutions.count)/\(startingPoints.count)")
+        }
+        return solutions.min()!
     }
 }
