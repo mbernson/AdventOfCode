d280ded88bd7c2283ae1bad448ca6039cf5935c4
Mathijs Bernson <mathijs.bernson@gmail.com>
2024-12-04 09:22:15 +0100

    2024 day 3 part 2

--- Sources/AdventOfCode2024/Day03.swift
+++ Sources/AdventOfCode2024/Day03.swift
@@ -11,4 +11,20 @@ struct Day03: AdventDay {
             }
             .reduce(0, +)
     }
+
+    func part2() throws -> Any {
+        var multiplicationEnabled = true
+        var total: Int = 0
+        for result in data.matches(of: /(?:(?:mul\((?<lhs>\d+),(?<rhs>\d+)\))|(?:do\(\))|(?:don\'t\(\)))/) {
+            print(result.output.0)
+            if let lhs = result.lhs, let rhs = result.rhs, multiplicationEnabled {
+                total += Int(lhs)! * Int(rhs)!
+            } else if result.output.0 == "do()" {
+                multiplicationEnabled = true
+            } else if result.output.0 == "don't()" {
+                multiplicationEnabled = false
+            }
+        }
+        return total
+    }
 }
--- Tests/AdventOfCode2024Tests/Day03.swift
+++ Tests/AdventOfCode2024Tests/Day03.swift
@@ -3,17 +3,19 @@ import Testing
 @testable import AdventOfCode2024
 
 struct Day03Tests {
-    let testData = """
-    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-    """
-
     @Test func testPart1() throws {
+        let testData = """
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        """
         let challenge = Day03(data: testData)
         #expect(String(describing: try challenge.part1()) == "161")
     }
 
     @Test func testPart2() throws {
-//        let challenge = Day03(data: testData)
-//        #expect(String(describing: challenge.part2()) == "32000")
+        let testData = """
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        """
+        let challenge = Day03(data: testData)
+        #expect(String(describing: try challenge.part2()) == "48")
     }
 }
