c75d6c5eda7915fe8fa4fd9d51a62d6acc50c642
Mathijs Bernson <mathijsb@q42.nl>
2025-12-08 23:39:31 +0100

    2025 day 8 part 1

---
+++ Sources/AdventOfCode2025/Day08.swift
@@ -0,0 +1,102 @@
+import Algorithms
+import Collections
+
+struct Day08: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+
+    // Splits input data into its component parts and convert from string.
+    var input: [Point] {
+        data.split(separator: "\n")
+            .filter { !$0.isEmpty }
+            .map {
+                let parts = $0.split(separator: ",")
+                return Point(x: Int(parts[0])!, y: Int(parts[1])!, z: Int(parts[2])!)
+            }
+    }
+
+    struct Point: Hashable, CustomStringConvertible {
+        let x, y, z: Int
+        var description: String { "\(x),\(y),\(z)"}
+    }
+    typealias Distance = Double
+    typealias Circuit = Set<Point>
+
+    func distanceBetween(p: Point, q: Point) -> Distance {
+        let (x, y, z) = (p.x - q.x, p.y - q.y, p.z - q.z)
+        return Double(x * x + y * y + z * z).squareRoot()
+    }
+
+    struct Pair: Hashable {
+        let a: Point
+        let b: Point
+
+        init(_ a: Point, _ b: Point) {
+            self.a = a
+            self.b = b
+        }
+
+        func hash(into hasher: inout Hasher) {
+            let ha = a.hashValue
+            let hb = b.hashValue
+            hasher.combine(ha &+ hb)
+            hasher.combine(ha &* hb)
+        }
+
+        static func == (lhs: Self, rhs: Self) -> Bool {
+            (lhs.a == rhs.a && lhs.b == rhs.b)
+                || (lhs.a == rhs.b && lhs.b == rhs.a)
+        }
+    }
+
+    func computeAllDistances() -> [Pair : Distance] {
+        let points = input
+        var distances: [Pair : Distance] = [:]
+        for point in points {
+            for other in points where other != point {
+                let pair = Pair(point, other)
+                if distances[pair] == nil {
+                    distances[pair] = distanceBetween(p: point, q: other)
+                }
+            }
+        }
+        return distances
+    }
+
+    func circuits(numberOfConnections: Int) -> [Circuit] {
+        let closestPairs = computeAllDistances()
+            .sorted(by: { lhs, rhs in
+                lhs.value < rhs.value
+            })
+            .map { $0.key }
+
+        var circuits: [Circuit] = input.map { Circuit([$0]) }
+
+        // Loop through the pairs from closest to furthest
+        for i in 0..<numberOfConnections {
+            let pair = closestPairs[i]
+            let circuitIndexA = circuits.firstIndex(where: { $0.contains(pair.a) })
+            let circuitIndexB = circuits.firstIndex(where: { $0.contains(pair.b) })
+            if let circuitIndexA, let circuitIndexB, circuitIndexA != circuitIndexB {
+                // Merge the circuits
+                let union = circuits[circuitIndexA].union(circuits[circuitIndexB])
+                let smallerIndex = min(circuitIndexA, circuitIndexB)
+                let largerIndex = max(circuitIndexA, circuitIndexB)
+                circuits[smallerIndex] = union
+                circuits.remove(at: largerIndex)
+            } else if circuitIndexA != nil || circuitIndexB != nil {
+                // Add the pair to the existing circuit
+                let circuitIndex = (circuitIndexA ?? circuitIndexB)!
+                circuits[circuitIndex].insert(pair.a)
+                circuits[circuitIndex].insert(pair.b)
+            }
+        }
+
+        return circuits
+    }
+
+    func part1() -> Any {
+        circuits(numberOfConnections: input.count == 20 ? 10 : 1000)
+            .map(\.count).max(count: 3).reduce(1, *)
+    }
+}
---
+++ Tests/AdventOfCode2025Tests/Day08.swift
@@ -0,0 +1,69 @@
+import Testing
+
+@testable import AdventOfCode2025
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+struct Day08Tests {
+    // Smoke test data provided in the challenge question
+    let testData = """
+    162,817,812
+    57,618,57
+    906,360,560
+    592,479,940
+    352,342,300
+    466,668,158
+    542,29,236
+    431,825,988
+    739,650,466
+    52,470,668
+    216,146,977
+    819,987,18
+    117,168,530
+    805,96,715
+    346,949,466
+    970,615,88
+    941,993,340
+    862,61,35
+    984,92,344
+    425,690,689
+    
+    """
+
+    @Test func testUnorderedHash() {
+        let a = Day08.Point(x: 1, y: 2, z: 4)
+        let a2 = Day08.Point(x: 1, y: 2, z: 4)
+        let b = Day08.Point(x: 4, y: 5, z: 6)
+        let c = Day08.Point(x: 7, y: 8, z: 9)
+        #expect(a != b)
+        #expect(a == a2)
+        #expect(a.hashValue == a2.hashValue)
+        #expect(Day08.Pair(a, b) == Day08.Pair(b, a))
+        #expect(Day08.Pair(a, b) == Day08.Pair(b, a))
+        #expect(Day08.Pair(a, b) != Day08.Pair(b, c))
+    }
+
+    @Test func testCircuits() {
+        let challenge = Day08(data: testData)
+        let circuits = challenge.circuits(numberOfConnections: 10).sorted(by: {
+            $0.count > $1.count
+        })
+        #expect(circuits.count == 11)
+        #expect(circuits[0].count == 5)
+        #expect(circuits[1].count == 4)
+        #expect(circuits[2].count == 2)
+        #expect(circuits[3].count == 2)
+        #expect(circuits[4].count == 1)
+        #expect(circuits[5].count == 1)
+        #expect(circuits[6].count == 1)
+        #expect(circuits[7].count == 1)
+        #expect(circuits[8].count == 1)
+        #expect(circuits[9].count == 1)
+        #expect(circuits[10].count == 1)
+    }
+
+    @Test func testPart1() {
+        let challenge = Day08(data: testData)
+        #expect(String(describing: challenge.part1()) == "40")
+    }
+}
